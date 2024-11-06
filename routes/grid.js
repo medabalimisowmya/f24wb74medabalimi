12ccb72c34812632cd2ac5cb69b8efdc7487e263
medabalimisowmya <medabalimisowmya@gmail.com>
2024-11-06 10:19:38 -0600

    created a matrice

--- routes/grid.js
+++ routes/grid.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-
-/* GET home page. */
+/* GET grid page with query parameters */
 router.get('/', function(req, res, next) {
-  let query = req.query; 
+  let query = req.query;
 
   // Log the rows and cols values to the console (for debugging)
   console.log(`rows: ${query.rows}`);
   console.log(`cols: ${query.cols}`);
-  res.render('grid', { title: 'Grid Display',query: query });
+  
+  // Render the grid.pug template and pass the query parameters
+  res.render('grid', { title: 'Grid Display', query: query });
 });
 
 module.exports = router;
